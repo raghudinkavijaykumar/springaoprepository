9208e3cc30baa87c141f453ed1a04506a4838b0f
Raghu Dinka Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-02-13 22:03:53 +0530

    Update with JpaRepositoryFactory

--- src/main/java/com/example/spingaoprepository/serializable/PipelineConfigurer.java
+++ src/main/java/com/example/spingaoprepository/serializable/PipelineConfigurer.java
@@ -1,13 +1,15 @@
 package com.example.spingaoprepository.serializable;
 
+import javax.persistence.EntityManager;
+
 import com.example.spingaoprepository.model.AllFile;
+import com.example.spingaoprepository.repository.AllFileRepository;
 import com.example.spingaoprepository.service.PipelineService;
 import com.example.spingaoprepository.utils.ApplicationContextUtils;
 
 import org.springframework.aop.framework.AopProxyUtils;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 public class PipelineConfigurer {
@@ -21,14 +23,22 @@ public class PipelineConfigurer {
     public void initialize() {
         AnnotationConfigApplicationContext applicationContext = (AnnotationConfigApplicationContext) ApplicationContextUtils
                 .getApplicationContext();
-        AutowireCapableBeanFactory autowireCapableBeanFactory = applicationContext.getAutowireCapableBeanFactory();
-        autowireCapableBeanFactory.autowireBean(pipelineService);
-        JpaRepository<AllFile, Long> jpaRepository = (SimpleJpaRepository<AllFile, Long>) AopProxyUtils
-                .getSingletonTarget(pipelineService.getAllFileRepository());
-
-        // Here i want an object of interface AllFileRepository
-        // jpaRepository.metadata is still a proxy. But i am not aware how to get object
-        // of my defined repository
+        // AutowireCapableBeanFactory autowireCapableBeanFactory =
+        // applicationContext.getAutowireCapableBeanFactory();
+        EntityManager em = applicationContext.getBean(EntityManager.class);
+        AllFileRepository allFileRepository = new JpaRepositoryFactory(em).getRepository(AllFileRepository.class);
+        allFileRepository.findByGuid(""); // Method is accessible here but the object is proxy
+        SimpleJpaRepository<AllFile, Long> simpleJpaRepository = (SimpleJpaRepository<AllFile, Long>) (AopProxyUtils
+                .getSingletonTarget(allFileRepository)); // But proxy resolution yeilds SimpleJpaRepository cannot
+                                                         // explicitly cast to AllFileRepository
+        ((AllFileRepository) simpleJpaRepository).findByGuid(""); // class
+                                                                  // org.springframework.data.jpa.repository.support.SimpleJpaRepository
+                                                                  // cannot be cast to class
+                                                                  // com.example.spingaoprepository.repository.AllFileRepository
+                                                                  // (org.springframework.data.jpa.repository.support.SimpleJpaRepository
+                                                                  // and
+                                                                  // com.example.spingaoprepository.repository.AllFileRepository
+                                                                  // are in unnamed module of loader 'app'
 
     }
 
