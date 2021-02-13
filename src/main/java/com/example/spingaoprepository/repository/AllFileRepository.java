d6c434af353f50ff988e3de93e9cc3bba5501c10
Raghu Dinka Vijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-02-13 17:39:12 +0530

    Added query

--- src/main/java/com/example/spingaoprepository/repository/AllFileRepository.java
+++ src/main/java/com/example/spingaoprepository/repository/AllFileRepository.java
@@ -3,10 +3,12 @@ package com.example.spingaoprepository.repository;
 import com.example.spingaoprepository.model.AllFile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AllFileRepository extends JpaRepository<AllFile, Long> {
+    @Query("SELECT a From AllFile a where a.guid = :guid")
     AllFile findByGuid(@Param("guid") String guid);
 }
