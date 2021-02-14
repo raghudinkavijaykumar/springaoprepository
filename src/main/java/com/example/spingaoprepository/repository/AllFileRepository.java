5705cbbc57dfb58173e5ab8bb5b22a666d4a4a95
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-02-14 09:36:21 +0700

    Provide AllFileRepository implementation
    This solves the casting problem in
    https://stackoverflow.com/q/66179155/1082681.

--- src/main/java/com/example/spingaoprepository/repository/AllFileRepository.java
+++ src/main/java/com/example/spingaoprepository/repository/AllFileRepository.java
@@ -4,9 +4,9 @@ import com.example.spingaoprepository.model.AllFile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-@Repository
+@NoRepositoryBean
 public interface AllFileRepository extends JpaRepository<AllFile, Long> {
     AllFile findByGuid(@Param("guid") String guid);
 }
--- src/main/java/com/example/spingaoprepository/serializable/PipelineConfigurer.java
+++ src/main/java/com/example/spingaoprepository/serializable/PipelineConfigurer.java
@@ -1,35 +1,23 @@
 package com.example.spingaoprepository.serializable;
 
-import com.example.spingaoprepository.model.AllFile;
+import com.example.spingaoprepository.repository.AllFileRepository;
 import com.example.spingaoprepository.service.PipelineService;
 import com.example.spingaoprepository.utils.ApplicationContextUtils;
 
 import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.context.ApplicationContext;
 
 public class PipelineConfigurer {
-
-    public PipelineConfigurer() {
-
-    }
-
-    PipelineService pipelineService = new PipelineService();
+    private ApplicationContext applicationContext = ApplicationContextUtils.getApplicationContext();
+    private PipelineService pipelineService = applicationContext.getBean(PipelineService.class);
 
     public void initialize() {
-        AnnotationConfigApplicationContext applicationContext = (AnnotationConfigApplicationContext) ApplicationContextUtils
-                .getApplicationContext();
         AutowireCapableBeanFactory autowireCapableBeanFactory = applicationContext.getAutowireCapableBeanFactory();
         autowireCapableBeanFactory.autowireBean(pipelineService);
-        JpaRepository<AllFile, Long> jpaRepository = (SimpleJpaRepository<AllFile, Long>) AopProxyUtils
+        AllFileRepository allFileRepository = (AllFileRepository) AopProxyUtils
                 .getSingletonTarget(pipelineService.getAllFileRepository());
-
-        // Here i want an object of interface AllFileRepository
-        // jpaRepository.metadata is still a proxy. But i am not aware how to get object
-        // of my defined repository
-
+        allFileRepository.findByGuid("XY-123");
     }
 
 }
